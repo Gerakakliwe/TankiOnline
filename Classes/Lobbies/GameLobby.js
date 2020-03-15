3652254e33a9226d012a33daf767d31797fa8b1f
Bogdan.Bondarenko <bogdan.bondarenko99@gmail.com>
2020-03-16 00:03:17 +0200

    Changing distance of collision bullets, 0.65 -> 1

--- Classes/Lobbies/GameLobby.js
+++ Classes/Lobbies/GameLobby.js
@@ -151,7 +151,7 @@ module.exports = class GameLobbby extends LobbyBase {
                 if(bullet.activator != player.id) {
                     let distance = bullet.position.Distance(player.position);
 
-                    if(distance < 0.65) {
+                    if(distance < 1) {
                         let isDead = player.dealDamage(50);
                         if(isDead) {
                             console.log('Player with id: ' + player.id + ' has died');
